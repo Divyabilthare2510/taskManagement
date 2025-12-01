6c4fced87756f52263b5a6c83ca331f82cd436fa
Divyabilthare2510 <divya.inurum@gmail.com>
2025-12-01 13:13:10 +0530

    deploy frontend in vercel  and beckend render.com change api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,7 +4,7 @@ const baseURL = import.meta.env.VITE_API_URL
   ? import.meta.env.VITE_API_URL 
   : import.meta.env.DEV 
     ? '/api' 
-    : 'http://localhost:5000/api';
+    : 'https://taskmanagement-mdjy.onrender.com/api';
 
 const api = axios.create({
   baseURL,
